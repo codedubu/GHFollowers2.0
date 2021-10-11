e2fa3972e717da003d693e5fc49b5eb17d3b40fb
codedubu <mccaine.river@gmail.com>
2021-10-10 17:17:01 -0700

    Final project before optimization.

--- GHFollowers2.0/Helpers and Extensions/Utilities/GFError.swift
+++ GHFollowers2.0/Helpers and Extensions/Utilities/GFError.swift
@@ -12,4 +12,6 @@ enum GFError: String, Error {
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
+    case unableToFavorite = "There wasn error favoriting this user. Please try again."
+    case alreadySavedToFavorites = "You already have this user on your favorites list!"
 } // END OF ENUM
